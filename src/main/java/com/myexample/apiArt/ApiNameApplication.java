fe449af8bf6caec25eb0c0ef926c01b053b429d1
Pavlo Karakata <pavlo.karakata@qvantel.com>
2020-08-28 13:53:46 +0300

    Initial commit

---
+++ src/main/java/com/myexample/apiArt/ApiNameApplication.java
@@ -0,0 +1,13 @@
+package com.myexample.apiArt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiNameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiNameApplication.class, args);
+	}
+
+}
